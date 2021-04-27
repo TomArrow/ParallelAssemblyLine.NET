f4a622ed8c1a9ba241a0ae3624ff27c34cf21c2b
Tom Arrow <davorfilms@googlemail.com>
2021-04-27 14:33:04 +0200

    should kinda work I guess, need to test still

--- ParallelAssemblyLine.NET/Class1.cs
+++ ParallelAssemblyLine.NET/Class1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace ParallelAssemblyLine.NET
 {
@@ -20,7 +22,7 @@ namespace ParallelAssemblyLine.NET
         /// <summary>
         /// Start a new assembly line. The function finishes once the digester has been called and has finished for the last item.
         /// </summary>
-        /// <param name="feeder">The feeder provides the source data which is to be processed. It will only get called single-threadedly and sequentially and provided with an incrementing number. You can use this as an index. If you wish to indicate that there are no more items to provide, return null.</param>
+        /// <param name="feeder">The feeder provides the source data which is to be processed. It will only get called single-threadedly and sequentially and provided with an incrementing number starting at zero. You can use this as an index. If you wish to indicate that there are no more items to provide, return null.</param>
         /// <param name="chewer">The chewer receives the data that the feeder provided, but multiple chewers work on multiple items in parallel.</param>
         /// <param name="digester">The digester receives, in sequential and single-threaded form, the output of the chewers. It can for example write this data sequentially into a file stream.</param>
         /// <param name="options">Options to define finer points of the behavior of the behavior of this function</param>
@@ -28,12 +30,54 @@ namespace ParallelAssemblyLine.NET
         {
             int threadCount = Environment.ProcessorCount;
             int bufferSize = threadCount * 2;
+            int mainLoopTimeOut = 500; // 500 ms
 
+            ConcurrentDictionary<Int64, TOut> processedData = new ConcurrentDictionary<long, TOut>();
+            ConcurrentDictionary<Int64, bool> threadsFinished = new ConcurrentDictionary<long, bool>();
 
+            Int64 nextToReadIndex = 0;
+            Int64 nextToDigestIndex = 0;
+            Int64 threadsRunning = 0;
             while (true)
             {
 
+                bool noMoreDataForWriting = false;
+                while (!noMoreDataForWriting && processedData.Count>0)
+                {
+                    if(threadsFinished.ContainsKey(nextToDigestIndex) && processedData.ContainsKey(nextToDigestIndex))
+                    {
+                        TOut resultForDigestion;
+                        bool success =  processedData.TryRemove(nextToDigestIndex, out resultForDigestion);
+                        if (success)
+                        {
 
+                            digester(resultForDigestion);
+                            nextToDigestIndex++;
+                        } else
+                        {
+                            noMoreDataForWriting = true;
+                        }
+
+                    }
+                }
+
+                // Only spawn new threads if buffer isn't full and full count of threads to run isn't exhausted.
+                while(threadsRunning < threadCount && processedData.Count < bufferSize)
+                {
+                    TIn inputData = feeder(nextToReadIndex);
+
+                    Int64 localIndex = nextToReadIndex; // Need to do this because otherwise the task will take the state of the more global variable and every thread will just access whatever.
+                    _ = Task.Run(()=> {
+                        TOut processedDataHere = chewer(inputData);
+                        processedData.TryAdd(localIndex, processedDataHere);
+                        processedDataHere = default(TOut);
+                        threadsFinished.TryAdd(localIndex, true);
+                    });
+
+                    nextToReadIndex++;
+                }
+
+                System.Threading.Thread.Sleep(mainLoopTimeOut);
             }
         }
     }
